966063da6830471bcf141e7dbfb893d24f728cf4
PolBarconsBou <polbarcons@gmail.com>
2017-10-26 16:21:31 +0200

    Ja es pot visualitzar la pregunta i les possibles respostes

--- app/src/main/java/edu/upc/eseiaat/pma/quiz/QuizActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/quiz/QuizActivity.java
@@ -2,12 +2,28 @@ package edu.upc.eseiaat.pma.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private int ids_answers[] = {
+            R.id.answer1, R.id.answer2, R.id.answer3, R.id.answer4
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        TextView text_question = (TextView) findViewById(R.id.text_question);
+        text_question.setText(R.string.question_content);
+
+        String[] answers = getResources().getStringArray(R.array.answers);
+
+        for (int i = 0; i < ids_answers.length; i++){
+            RadioButton rb = (RadioButton) findViewById(ids_answers[i]);
+            rb.setText(answers[i]);
+        }
     }
 }
